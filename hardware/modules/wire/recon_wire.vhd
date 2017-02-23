4b8314c7172212d83d44d8482327cc4d45c777c2
Luca Pezzarossa <lpez@dtu.dk>
2017-02-23 11:06:30 +0100

    Adding modules.

---
+++ hardware/modules/wire/recon_wire.vhd
@@ -0,0 +1,20 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+entity recon_wire is
+	port(
+		led_in   : in std_logic_vector(7 downto 0);
+		led_out   : out std_logic_vector(7 downto 0)
+	);
+end recon_wire;
+
+architecture rtl of recon_wire is
+
+begin
+
+led_out <= led_in;
+
+end rtl;
